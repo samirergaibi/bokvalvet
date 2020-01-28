bf4b29c0637e9cbe05280908ee7d533a57082d3d
Samir Ergaibi <samirergaibi@gmail.com>
2020-01-28 08:55:53 +0100

    feat(): book page, styled it alittle aswell

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -16,6 +16,9 @@ const Book = ({ id }) => {
       console.log(resp.volumeInfo);
     });
   }, [id]);
+  // if (book) {
+  //   book.authors = ["Samir Ergaibi", "Elin Säfström", "Solveig Ergaibi"];
+  // }
 
   return (
     <Fragment>
@@ -26,27 +29,64 @@ const Book = ({ id }) => {
           }}
         >
           <h1>{book.title}</h1>
-          {book.authors &&
-            book.authors.map((author, i) => (
-              <p key={`${author}-${i}`}>{author}</p>
-            ))}
-          <p>
-            <FontAwesomeIcon icon={["far", "calendar-alt"]} />
-            {dateConverter(book.publishedDate)}
-          </p>
-          <p>
-            <FontAwesomeIcon icon="book" />
-            {book.pageCount}
-          </p>
-          {book.categories &&
-            book.categories.map((category, i) => (
-              <p key={`${category}-${i}`}>{category}</p>
-            ))}
-            {
-              book.imageLinks ?
-              <img src={book.imageLinks.thumbnail} alt={`${book.title}`} /> :
-              <img src={noImageFound} alt={`${book.title}`} />
-            }
+          {book.imageLinks ? (
+            <img src={book.imageLinks.thumbnail} alt={`${book.title}`} />
+          ) : (
+            <img src={noImageFound} alt={`${book.title}`} />
+          )}
+          <div>
+            <div css={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "start  "
+            }}>
+              <p css={{margin: "0 5px 0 0"}}>av:</p>
+              <div css={{
+                display: "flex",
+                flexDirection: "column",
+              }}>
+                {book.authors &&
+                  book.authors.map((author, i) => (
+                    <a
+                      key={`${author}-${i}`}
+                      href={`https://www.google.se/search?q=${author}`}
+                      target="_blank"
+                      rel="noreferrer"
+                      rel="noopener"
+                      css={{
+                        textDecoration: "none",
+                        color: "#0000EE"
+                      }}
+                    >
+                      {author}
+                    </a>
+                  ))}
+              </div>
+            </div>
+            <div
+              css={{
+                display: "flex",
+                justifyContent: "space-around",
+                "& p": {
+                  fontFamily: "'Lato', sans-serif"
+                },
+                "& svg": {
+                  marginRight: "10px",
+                  color: "#a46e0d"
+                  // color: "#d6be97"
+                }
+              }}
+            >
+              <p>
+                <FontAwesomeIcon icon={["far", "calendar-alt"]} />
+                {dateConverter(book.publishedDate)}
+              </p>
+              <p>
+                <FontAwesomeIcon icon="book" />
+                {book.pageCount}
+              </p>
+            </div>
+          </div>
           <div dangerouslySetInnerHTML={{ __html: book.description }}></div>
         </div>
       ) : (
