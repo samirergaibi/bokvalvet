b8a302f1fa15cef748dd1d3320c414ac8266e43c
Samir Ergaibi <samirergaibi@gmail.com>
2020-02-13 13:16:08 +0100

    feat(): added logo and favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
 import { Fragment, useState } from "react";
+import { Link } from "@reach/router";
 
 import Nav from "./Nav";
 import HamburgerButton from "../components/HamburgerButton";
 import mq from "../utils/mediaQueries";
+import Logo from "../images/logo.png";
 
 const Layout = ({ children }) => {
 
@@ -33,6 +35,16 @@ const Layout = ({ children }) => {
               display: "none"
             }
           }}>
+          <Link to="/" >
+            <img src={Logo} alt="Logo" css={{
+              width: "50px",
+              position: "absolute",
+              top: "5px",
+              left: "10px",
+              zIndex: "2",
+              backfaceVisibility: "hidden"
+            }} />
+          </Link>
           <HamburgerButton isClosed={isClosed} setIsClosed={setIsClosed} />
         </div>
         <Nav isClosed={isClosed} setIsClosed={setIsClosed} />
